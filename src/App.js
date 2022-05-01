4e165ddfd16a1504e20e14f68c2e848e6c65efc4
kolera75 <amine.sebki@gmail.com>
2022-05-01 17:52:48 +0200

    Adding icons of the tab and adding routes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,70 @@
+/*import TroisiemeComposant, { PremierComposant, ComposantBonjour} from "./components/ui/PremierComposant/PremierComposant"*/
 import logo from './logo.svg';
+
 import './App.css';
+import ComposantHeader from "./components/container/ComposantHeader/ComposantHeader"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/page/Home/Home';
+import Profil from './components/page/Profil/Profil';
+import Routine from './components/page/Routine/Routine';
+import Counter from './components/page/Counter/Counter';
+import Auth from "./components/page/Auth/Auth";
+import {userContext} from "./components/page/Auth/Auth";
+
 
 function App() {
 
-  let utilisateur = { prenom: "Amin", nom: "Sebki"};
+  let utilisateur = { 
+  prenom: "Amin",
+   nom: "Sebki",
+   cours: [
+    {
+      nom: "HTML",
+      isDone: true,
+      icon: "https://img.icons8.com/color/344/html-5--v1.png",
+    },
+    {
+      nom: "CSS",
+      isDone: true,
+      icon: "https://img.icons8.com/color/344/css3.png",
+    },
+    {
+      nom: "Javascript",
+      isDone: true,
+      icon: "https://img.icons8.com/color/344/javascript--v1.png",
+    },
+    {
+      nom: "React",
+      isDone: false,
+      icon: "https://img.icons8.com/officel/344/react.png",
+    },
+  ],
+};
 
-  function PremierComposant(){
-    return <p>Notre premier composant!</p>
-    }
-
-    function ComposantBonjour({user, isPermis}){
-      return (
-      <div>
-        <span> {user.prenom} </span>
-        <span>{user.nom}</span>
-        <p>{isPermis ? "Vous pouvez conduire" : "Prendre le bus"}</p>
-        </div>
-      );
-    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Bonjour et bienvenue sur React !</p>
-        <p>{utilisateur.prenom}</p>
-        { PremierComposant()}
-        <PremierComposant />
+      <BrowserRouter>
+      <ComposantHeader title="Une application avec React!"/>
 
-       {/*} { ComposantBonjour(utilisateur)}*/}
-        <ComposantBonjour 
-        user={utilisateur} isPermis={true}
-        />
-      </header>
+
+      <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/profil" element={<Profil user={utilisateur}/>}/>
+      <Route path="/routine" element={<Routine/>}/>
+      <Route path="/counter" element={<Counter/>}/>
+      <Route path='/auth' element={<Auth />} />
+
+      </Routes>
+
+
+      </BrowserRouter>
+      
+
+
+
+      {/*<PremierComposant />
+      <ComposantBonjour user={utilisateur} isPermis={false} />
+  <TroisiemeComposant></TroisiemeComposant>*/}
     </div>
   );
 }
